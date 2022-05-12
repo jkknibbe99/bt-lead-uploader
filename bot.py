8b0ddc02d71c9f09336998afbc4193f61ada6a28
Josh <jkknibbe99@gmail.com>
2022-05-12 09:41:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import WebDriverException, NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException, TimeoutException
 
 
 # Initialize globals
@@ -53,39 +54,27 @@ def login():
 def importLeads():
     # Click import leads button
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rc-tabs-0-panel-ListView"]/header/a[2]/button'))).click()
-    
+    # Select file to upload
     file_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ImportWizard .UploadButton input')))
     file_input.send_keys(get_config_data(DataCategories.INIT_DATA, 'leads_file_path'))
-
-
-# Request a bulk export
-def requestBulkExport():
-    # Click Settings dropdown button
-    settings_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="reactMainNavigation"]/div[1]/div/ul/li[12]/div')))
-    actionChains.move_to_element(settings_btn).click().perform()
-    # Find and click Setup dropdown option
-    dropdown_div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@id="reactMainNavigation"]/div[1]/div[1]/div')))
-    setup_elem = dropdown_div.find_element(By.XPATH, '//div/div/ul/li[1]/span/div/a/div/div[text()="Setup"]')
-    setup_elem = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(setup_elem))
-    actionChains.move_to_element(setup_elem).click().perform()
-    # Switch to Setup iframe
-    iframes = driver.find_elements(By.TAG_NAME, 'iframe')
-    for iframe in iframes:
-        if iframe.get_dom_attribute('title') == 'Setup':
-            setup_iframe = iframe
-            break
-    driver.switch_to.frame(setup_iframe)
-    # Click Bulk Export button
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/section/main/div/div[2]/div[1]/div[2]/div/div[8]/a/div'))).click()
-    # Select Open and Closed Jobs
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/section/main/div[2]/div[1]/div[1]/div/div/div[2]/form/div[1]/div/div/label[2]/span[1]'))).click()
-    # Click on the Request Export button
-    export_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/section/main/div[2]/div[1]/div[1]/div/div/div[2]/form/div[3]/div/div[1]/div/button/span')))
-    if export_btn.text == 'Cancel Export':
-        newStatus('Attempted to request a bulk export, however, it looks like one was already requested.', True)
+    # Click 'Next' button until the results page reached
+    while True:
+        try:
+            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.ImportWizard [data-testid="next"]'))).click()
+        except StaleElementReferenceException:
+            try:
+                driver.find_element(By.CSS_SELECTOR, '.ImportWizard .importWizardResults')
+            except NoSuchElementException:
+                pass
+            else:
+                break
+    # Check if import was successful
+    try:
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ImportWizard .success-message')))
+    except NoSuchElementException:
+        newStatus('Import was not successful!', True)
     else:
-        actionChains.move_to_element(export_btn).click().perform()
-        newStatus('Bulk export requested successfully', False)
+        newStatus('Import was successful!', False)
 
 
 # Close chrome driver bot
@@ -97,10 +86,13 @@ def closeChrome():
 
 # Main method
 if __name__ == '__main__':
-    initDriver()
-    initActionChains()
-    login()
-    importLeads()
-    time.sleep(2)
-    closeChrome()
+    try:
+        initDriver()
+        initActionChains()
+        login()
+        importLeads()
+        time.sleep(2)
+        closeChrome()
+    except Exception as e:
+        newStatus('Exception raised while running program:\n' + str(e), True)
     sys.exit()
