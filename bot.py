1398bab4c4330a98c55473bad480500a5a68db19
Josh <jkknibbe99@gmail.com>
2022-05-13 14:34:19 -0400

    program 90%

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 # Imports
-from config import DataCategories, get_config_data #login_data, chromedriver_data
-from bot_status import newStatus
-import os, sys, time
+import os, sys, shutil, datetime, re, time
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import WebDriverException, NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementClickInterceptedException, TimeoutException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from config import DataCategories, get_config_data
+from bot_status import newStatus
 
 
 # Initialize globals
@@ -15,22 +18,35 @@ driver = None
 chrome_options = None
 actionChains = None
 
+# Browser to be used
+BROWSER = 'Undetected Chrome'  # Options: 'Chrome', 'Undetected Chrome', 'Firefox'
+
 
 # Initialize chrome driver
 def initDriver():
-    # Get path to chromedriver
-    if os.name == 'nt':  # Windows
-        directory = get_config_data(DataCategories.CHROMEDRIVER_DATA, 'directory').replace('/', '\\')
-        chromedriver_path = (os.path.realpath(__file__)[::-1][(os.path.realpath(__file__)[::-1].find('\\')+1):])[::-1] + directory + 'chromedriver_100_win.exe'
-        print(chromedriver_path)
-    else:  # Mac
-        raise NotImplementedError
-    # Declare chromedriver
+    # Declare driver
     global driver
-    if chrome_options is None:
+    # Chrome
+    if BROWSER == 'Chrome':
+        cd_directory = get_config_data(DataCategories.CHROMEDRIVER_DATA, 'directory').replace('/', '\\')
+        chromedriver_path = os.path.realpath(__file__) + '\\' + cd_directory + 'chromedriver_100_win.exe'
         driver = webdriver.Chrome(executable_path=chromedriver_path)
+    if BROWSER == 'Undetected Chrome':
+        driver = uc.Chrome()
+    # Firefox
+    elif BROWSER == 'Firefox':
+        try:
+            profile = webdriver.FirefoxProfile(get_config_data(DataCategories.FIREFOXDRIVER_DATA, 'profile_path'))
+            profile.set_preference('dom.webdriver.enabled', False)
+            profile.set_preference('useAutomationExtension', False)
+            profile.update_preferences()
+            desired = DesiredCapabilities.FIREFOX
+            driver = webdriver.Firefox(firefox_profile=profile, desired_capabilities=desired)
+        except Exception as e:
+            print(str(e))
     else:
-        driver = webdriver.Chrome(executable_path=chromedriver_path,options=chrome_options)
+        raise ValueError('Not set up for this browser yet (' + BROWSER + ')')
+    driver.maximize_window()
 
 
 # Initialize actionChains. This is used to perform actions like scroling elements into view
@@ -39,8 +55,44 @@ def initActionChains():
     actionChains = ActionChains(driver)
 
 
+# Download leads file using link
+def downloadLeadsFile():
+    # Delete leads file before new download
+    try:
+        os.remove(get_config_data(DataCategories.LEADS_DATA, 'leads_filepath'))
+    except Exception as e:
+        print(str(e))
+    # Download new leads file
+    driver.get(get_config_data(DataCategories.LEADS_DATA, 'leads_file_download_url'))
+    i = 0
+    num_itrs = 1000
+    for i in range(num_itrs):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('Checking for download...')
+        if os.path.exists(get_config_data(DataCategories.LEADS_DATA, 'leads_filepath')):
+            break
+    if i == num_itrs-1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('Leads file failed to download.')
+        input('Press [ENTER] to close program ')
+        sys.exit()
+    # Copy leads file to archive location. Append timestamp to filename
+    timestamp = '_' + str(datetime.datetime.now()).replace('-','').replace(' ','_').replace(':','')
+    timestamp = timestamp[:timestamp.find('.')]
+    try:
+        src_filepath = get_config_data(DataCategories.LEADS_DATA, 'leads_filepath')
+        filename = os.path.basename(src_filepath)
+        dst_filepath = get_config_data(DataCategories.LEADS_DATA, 'leads_archive_directory') + '\\' +filename[:filename.find('.')] + timestamp + '.csv'
+        shutil.copyfile(src_filepath, dst_filepath)
+    except Exception as e:
+        print('-- ERROR --')
+        print(str(e))
+        input('Press [ENTER] to close program ')
+        sys.exit()
+
+
 # Log into BuilderTrend
-def login():
+def btLogin():
     # Go to the login url
     driver.get(get_config_data(DataCategories.BT_LOGIN_DATA, 'login_url'))
     # Log in
@@ -51,17 +103,18 @@ def login():
     actionChains.move_to_element(login_btn).click().perform()
 
 
-def importLeads():
+# Upload leads file to BuilderTrend
+def btUploadLeads():
     # Click import leads button
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rc-tabs-0-panel-ListView"]/header/a[2]/button'))).click()
     # Select file to upload
     file_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ImportWizard .UploadButton input')))
-    file_input.send_keys(get_config_data(DataCategories.INIT_DATA, 'leads_file_path'))
+    file_input.send_keys(get_config_data(DataCategories.LEADS_DATA, 'leads_filepath'))
     # Click 'Next' button until the results page reached
     while True:
         try:
             WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.ImportWizard [data-testid="next"]'))).click()
-        except StaleElementReferenceException:
+        except (StaleElementReferenceException, ElementClickInterceptedException): 
             try:
                 driver.find_element(By.CSS_SELECTOR, '.ImportWizard .importWizardResults')
             except NoSuchElementException:
@@ -72,16 +125,112 @@ def importLeads():
     try:
         WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ImportWizard .success-message')))
     except NoSuchElementException:
-        newStatus('Import was not successful!', True)
+        # newStatus('Import was not successful!', True)
+        print('Import was NOT successful')
+        input('Press [ENTER] ')
+        sys.exit()
     else:
-        newStatus('Import was successful!', False)
+        # newStatus('Import was successful!', False)
+        print('Import was successful!')
+
+
+# Login to EH gmail account
+def googleLogin():
+    try:
+        driver.get('https://accounts.google.com/')
+        # enter email and password
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#identifierId'))).send_keys(get_config_data(DataCategories.EH_GMAIL_LOGIN_DATA, 'email'))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#identifierNext button'))).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#password input'))).send_keys(get_config_data(DataCategories.EH_GMAIL_LOGIN_DATA, 'password'))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#passwordNext button'))).click()
+    except Exception as e:
+        print('ERROR: Could not log into google account')
+        raise e
+
+
+# clicks the clear/reset buttons in the Lead google sheets
+def clearSheets():
+    ## Lead Sheet 1
+    # Open new lead sheet 1
+    new_lead_sheet_1_url = get_config_data(DataCategories.LEADS_DATA, 'new_lead_sheet_1_url')
+    while not re.search(new_lead_sheet_1_url+'.*',driver.current_url):
+        print('trying url...')
+        driver.get(new_lead_sheet_1_url)
+    print('url reached')
+    # Click 'Clear Sheet' button
+    while True:
+        print('attempting click')
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#fixed_right .waffle-borderless-embedded-object-container div:nth-child(1)'))).click()
+        except ElementClickInterceptedException:
+            try:
+                driver.find_element(By.CSS_SELECTOR, 'div.docs-error-dialog button').click()
+            except NoSuchElementException:
+                print('Could not click Clear Sheet button on the first sheet, something is blocking it and it is not the sign-in dialog.')
+                input('Press [ENTER] to close program ')
+                closeChrome()
+        else:
+            try:
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@id="docs-butterbar-container"]/div/div[@role="alert" and text()="Finished script"]')))
+                break
+            except TimeoutException:
+                pass
+    
+    ## Lead Sheet 2
+    # Open new lead sheet 2
+    new_lead_sheet_2_url = get_config_data(DataCategories.LEADS_DATA, 'new_lead_sheet_2_url')
+    while not re.search(new_lead_sheet_2_url+'.*',driver.current_url):
+        print('trying url...')
+        driver.get(new_lead_sheet_2_url)
+    print('url reached')
+    # Click 'Clear Sheet' button
+    while True:
+        print('attempting click')
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#fixed_right .waffle-borderless-embedded-object-container div:nth-child(1)'))).click()
+        except ElementClickInterceptedException:
+            try:
+                driver.find_element(By.CSS_SELECTOR, 'div.docs-error-dialog button').click()
+            except NoSuchElementException:
+                print('Could not click Clear Sheet button on the second sheet, something is blocking it and it is not the sign-in dialog.')
+                input('Press [ENTER] to close program ')
+                closeChrome()
+        else:
+            try:
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@id="docs-butterbar-container"]/div/div[@role="alert" and text()="Finished script"]')))
+                break
+            except TimeoutException:
+                pass
+
+
+    ## Lead Sheet 3
+    # Open new lead sheet 3
+    new_lead_sheet_3_url = get_config_data(DataCategories.LEADS_DATA, 'bt_lead_upload_sheet_url')
+    while not re.search(new_lead_sheet_3_url+'.*',driver.current_url):
+        print('trying url...')
+        driver.get(new_lead_sheet_3_url)
+    print('url reached')
+    # Click 'ResetSheet' dropdown
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#docs-extensions-menu'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span[aria-label="Macros m"]'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="ResetSheet"]'))).click()
+    time.sleep(2)
+
+
+# Stop the program
+def stop():  # TODO: remove this and all instances after dev
+    print('Program stopped')
+    input('Press [ENTER] to close program ')
+    closeChrome()
 
 
 # Close chrome driver bot
 def closeChrome():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print('\n\nChromedriver Closed\n')
+    os.system('cls' if os.name == 'nt' else 'clear')  #TODO: Uncomment when dev finished
+    print('Closing driver...')
     driver.quit()
+    print('\n\nDriver Closed\n')
+    sys.exit()
 
 
 # Main method
@@ -89,10 +238,15 @@ if __name__ == '__main__':
     try:
         initDriver()
         initActionChains()
-        login()
-        importLeads()
-        time.sleep(2)
+        downloadLeadsFile()
+        btLogin()
+        btUploadLeads()
+        googleLogin()
+        clearSheets()
         closeChrome()
     except Exception as e:
-        newStatus('Exception raised while running program:\n' + str(e), True)
+        # newStatus('Exception raised while running program:\n' + str(e), True)
+        print('Error encountered')
+        input('Press [ENTER] to view')
+        raise e
     sys.exit()
