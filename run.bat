d014320dadc8805c2a60abcb32ec903cf4dc735d
Josh <jkknibbe99@gmail.com>
2022-06-27 10:16:54 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,14 +9,14 @@ rem check if a venv exists
 if exist Scripts (
     echo Scripts directory exists
     echo Running bot
-    call %~dp0\Scripts\activate
+    call %~dp0Scripts\activate
     rem run bot
     python %~dp0bot.py
 ) else (
     echo Virtual environment not created.
     echo Creating Virtual environment now...
     python -m venv %~dp0
-    call %~dp0\Scripts\activate
+    call %~dp0Scripts\activate
     pip install -r requirements.txt
     rem run bot
     python %~dp0bot.py
